bc275906b112808f37da3f67786ffd1c50434198
Adrian Cacho <agcacho22@gmail.com>
2022-09-03 19:08:17 -0700

    fix card layout

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,46 @@
+import {
+    Card as CardBase,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Typography,
+} from '@material-tailwind/react'
+import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Project } from '../types/Project'
+
+interface ProjectCardProps {
+    project: Project
+}
+
+export default function ProjectCard({ project }: ProjectCardProps) {
+    const { name, image, link, role } = project
+    return (
+        <Link to={link}>
+            <CardBase className=" w-56">
+                <CardHeader floated={false} className=" h-32 w-32 mx-auto">
+                    <img
+                        className="object-cover w-full h-full object-center"
+                        src={`/images/${image}`}
+                        alt="profile-picture"
+                    />
+                </CardHeader>
+                <CardBody className="text-center">
+                    <Typography variant="h4" color="blue-gray" className="mb-2">
+                        {name}
+                    </Typography>
+                    <Typography
+                        color="blue"
+                        className="font-medium"
+                        textGradient
+                    >
+                        {role}
+                    </Typography>
+                </CardBody>
+                {/* <CardFooter className="flex justify-center gap-7 pt-2">
+                    <Typography>{role}</Typography>
+                </CardFooter> */}
+            </CardBase>
+        </Link>
+    )
+}
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,11 +1,38 @@
 import { Typography } from '@material-tailwind/react'
-import ProjectCard from '../Card'
+import { graphql, useStaticQuery } from 'gatsby'
+import { Project } from '../../types/Project'
+import ProjectCard from '../ProjectCard'
 
 export default function Projects() {
+    const data = useStaticQuery<{
+        site: { siteMetadata: { projects: Project[] } }
+    }>(graphql`
+        query ProjectsQuery {
+            site {
+                siteMetadata {
+                    projects {
+                        name
+                        image
+                        link
+                        role
+                    }
+                }
+            }
+        }
+    `)
+    const {
+        site: {
+            siteMetadata: { projects },
+        },
+    } = data
     return (
-        <section>
+        <section className="">
             <Typography variant="h2">Projects</Typography>
-            <ProjectCard />
+            <div className="flex flex-nowrap gap-4 overflow-x-auto md:flex-wrap md:overflow-x-hidden">
+                {projects.map((project) => (
+                    <ProjectCard project={project} />
+                ))}
+            </div>
         </section>
     )
 }
