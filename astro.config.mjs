b2340f3429a2047f1d54f8b25ca264ea6dbc2530
Adrian <adrianc@meetparcel.com>
2022-11-26 18:36:30 -0600

    add daisy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cachoe.dev',
+  site: 'https://cacho.dev',
   integrations: [mdx(), sitemap(), tailwind()]
 });
