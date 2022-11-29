db5427693f0a8c2d20b98c2627e78ab571dd787b
Adrian <adrianc@meetparcel.com>
2022-11-29 16:47:32 -0600

    layout fix

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,34 @@
+import { Fade, Slide } from "react-reveal";
+
+export default function () {
+  return (
+    <div className="w-full items-center justify-center flex gap-8 my-8 flex-col md:flex-row">
+      <Fade bottom distance="50px">
+        <div className="avatar">
+          <div className="w-32 rounded-full brightness-[1.1] saturate=[1.1]">
+            <img src="/pfp.jpeg" />
+          </div>
+        </div>
+      </Fade>
+
+      <div className="prose flex flex-col">
+        <Fade right duration={1000} distance={"200px"}>
+          <h3 className="m-0">Hey there!👋</h3>
+        </Fade>
+        <Fade right duration={1000} distance={"200px"} delay={"600"}>
+          <h2 className="m-0">I'm Adrian</h2>
+        </Fade>
+        <Fade right duration={1000} distance={"200px"} delay={"1600"}>
+          <h3 className="m-0">(But you can call me Cacho)</h3>
+        </Fade>
+        <Fade cascade right delay={"3000"}>
+          <div className="flex flex-col  md:gap-[1ch]md:flex-row">
+            <h3 className="m-0">Visual Artist 👾</h3>
+            <h3 className="m-0">Designer 🧩</h3>
+            <h3 className="m-0">Developer 🛠️</h3>
+          </div>
+        </Fade>
+      </div>
+    </div>
+  );
+}
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,10 @@
+import Card from "../components/Card";
+
+export default function () {
+  return (
+    <div className="w-full flex flex-col items-center justify-center prose">
+      <h2>Projects</h2>
+      <Card />
+    </div>
+  );
+}
