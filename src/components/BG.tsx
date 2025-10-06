e9d9ef00b6ee2d901cfc2b3dec01755c61c4652e
Adrian <agcacho22@gmail.com>
2025-10-05 21:34:21 -0700

    make card the link

--- src/components/BG.tsx
+++ src/components/BG.tsx
@@ -3,18 +3,6 @@ import Silk from "./Silk";
 export default function Background() {
   return (
     <div className="fixed w-full h-full top-0 left-0 z-[-1]">
-      {/* <PrismaticBurst
-        animationType="rotate3d"
-        intensity={2}
-        speed={0.5}
-        // distort={1.0}
-        // paused={false}
-        offset={{ x: 0, y: 0 }}
-        hoverDampness={0}
-        rayCount={24}
-        mixBlendMode="lighten"
-        colors={["#f2ff38", "#bd55fa", "#ff4e3b", "#29e2ff"]}
-      /> */}
       <Silk
         speed={7.6}
         scale={1}
