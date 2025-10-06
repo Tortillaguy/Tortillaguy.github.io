6c702d086a4b8205373aac333dc726a633856872
Adrian <agcacho22@gmail.com>
2025-10-05 18:30:53 -0700

    use silk bg;

--- src/components/BG.tsx
+++ src/components/BG.tsx
@@ -1,9 +1,9 @@
-import PrismaticBurst from "./PrismaticBurst";
+import Silk from "./Silk";
 
 export default function Background() {
   return (
     <div className="fixed w-full h-full top-0 left-0 z-[-1]">
-      <PrismaticBurst
+      {/* <PrismaticBurst
         animationType="rotate3d"
         intensity={2}
         speed={0.5}
@@ -14,6 +14,13 @@ export default function Background() {
         rayCount={24}
         mixBlendMode="lighten"
         colors={["#f2ff38", "#bd55fa", "#ff4e3b", "#29e2ff"]}
+      /> */}
+      <Silk
+        speed={7.6}
+        scale={1}
+        color="#5227FF"
+        noiseIntensity={5.5}
+        rotation={0}
       />
     </div>
   );
